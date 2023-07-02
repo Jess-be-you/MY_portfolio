b553d245623c3d815af2b6a5a3297116a0507bae
Jess-be-you <jesmipj001@gmail.com>
2023-07-03 03:20:48 +0530

    left and right side committed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Navbar from './pages/Navbar';
+import LeftSide from './pages/LeftSide';
+import RightSide from './pages/RightSide';
 import './App.css';
 
 function App() {
   return (
     <div className=" w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+          <LeftSide />
+        </div>
+        <div className="h-88vh">
+          middle
+        </div>
+        <div className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+          <RightSide />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/RightSide.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightSide = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
+        <a href="jesmijose282@gmail.com">
+            <p className="text-sm rotate-90 w-72 tracking-wide text-textGreen">Jesmi_pj.email.me</p>
+        </a>
+        <div className='w-[2px] h-32 bg-textDark'></div>
+    </div>
+    
+  )
+}
+
+export default RightSide
