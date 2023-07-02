84073b6a1699b72fd8a4718a86a85765106bf74d
Jess-be-you <jesmipj001@gmail.com>
2023-07-03 02:27:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './pages/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className=" w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
